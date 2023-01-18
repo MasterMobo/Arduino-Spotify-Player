5ba2af6af3376dbdf3829fe0ce6307b6c690759e
Netherheim <122715291+Netherheim@users.noreply.github.com>
2023-01-18 12:12:25 +0700

    Play a specific song

--- Voice_reg.py
+++ Voice_reg.py
@@ -20,6 +20,8 @@ commandWords = {
     ("last", "previous", "go back"):spotify.previous_track,
     ("shuffle","randomize"):shuffleCheck,
     ("repeat", "loop"):repeatCheck,
+    "play": getSong
+    
 }
 
 def shuffleCheck():
@@ -35,6 +37,14 @@ def repeatCheck():
         spotify.repeat("track")
     elif "album" in words or "playlist" in words:
         spotify.repeat("context")
+        
+def getSong():  # This function will slice the initial commands to get Artist - Song (Some songs have identical names)
+    song_artist = words[5:]  # Get both Song and artist
+    song = song_artist.split(" ")[0]
+    artist = song_artist.split(" ")[-1]
+    track_id = spotify.search(q="artist:" + artist + " track:" + song, type="track")
+    song_link = "https://api.spotify.com/v1/tracks/{id}".format(id=track_id)
+    spotify.start_playback(uris=song_link)
         
 
 print("Finished Initializing!")
